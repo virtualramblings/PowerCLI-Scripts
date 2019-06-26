44ce930d150ee2b8156a308ba66c2097e5b62dca
virtualramblings <48842033+virtualramblings@users.noreply.github.com>
2019-06-26 16:21:58 -0400

    Create set-snmp.ps1

---
+++ set-snmp.ps1
@@ -0,0 +1,107 @@
+<#
+
+    .Description
+        Script to modify SNMP settings
+
+    .Changelog
+        2/25/2019 - Added menu for cluster selection
+        2/25/2019 - Removed SNMP Targets
+        2/25/2019 - Staticly set $community variable
+        2/27/2019 - v2.1 Rewrote Set SNMP to use esxcli -V2
+
+    .Testing
+        2/22/2019 - Validated SNMP settings take effect immediately (no service restart required)
+        2/25/2019 - Validated against 6.5/6.7 (snmpwalk -v 2c -c $community $vmhost)
+        2/27/2019 - Validated set/remove targets
+
+#>
+
+# Add the VMware Module
+if (!(Get-Module VMware.VimAutomation.Core)) {
+    Import-Module VMware.VimAutomation.Core
+}
+
+# Parameters
+$outputdir = "C:\Support\snmp"
+$date = Get-Date -Format M-d-yy
+
+# Validate folders
+If (Test-Path -Path $outputdir) {
+} else {
+New-Item -ItemType Directory -Path $outputdir
+}
+
+# Connect to vCenter
+$vcenter = Read-Host “Enter vCenter FQDN“
+
+Connect-VIServer -Server $vcenter
+
+$serverlist = $global:DefaultVIServer
+
+if ($serverlist -eq $null) {
+    BREAK
+    } else {
+}
+
+# Set SNMP variables
+$community = "entstringp"
+
+# Run stuff
+Do {
+    Write-Host "`nPress 'm' to Modify SNMP or press 'q' to Quit`n"
+
+    $input = Read-Host "Select"
+    switch ($input){
+        'm' {          
+            
+            Write-Host "`nHere are the clusters for $vcenter"
+            # Create dynamic menu for Cluster selection
+            $global:i=0
+            $menu = Get-Cluster | Select @{Name="Line";Expression={$global:i++;$global:i}},Name
+            $menu | Format-Table -AutoSize
+            $r = Read-Host "Select cluster"
+            $clsname = $menu | ? {$_.Line -eq "$r"} | Select -ExpandProperty Name
+
+            # Get all hosts in vCenter managed Cluster so we can cycle thru them
+            $hosts = Get-Cluster -Name $clsname | Get-VMHost
+
+            ForEach ($vmhost in $hosts) {
+            $esxcli = Get-EsxCli -VMHost $vmhost -V2
+
+            # Modify SNMP
+            $snmpargs = $esxcli.system.snmp.set.CreateArgs()
+            $snmpargs.reset = "true"
+            $snmpargs.enable = "true"
+            $snmpargs.communities = $community
+            #$snmpargs.targets = "172.25.8.2/public"
+            $esxcli.system.snmp.set.Invoke($snmpargs)
+            }
+
+            # Output SNMP Settings
+            $result = @()
+            ForEach ($vmhost in $hosts) {
+
+            # Get current SNMP settings
+            $snmpget = $esxcli.system.snmp.get.Invoke()
+
+            $result += [PSCustomObject] @{
+                Hostname = $vmhost
+                Enabled = "$($snmpget | Select -ExpandProperty Enable)" 
+                Port = "$($snmpget | select -ExpandProperty Port)"
+                Communities = "$($snmpget | Select -ExpandProperty Communities)"
+                #Targets = "$($snmpget | Select -ExpandProperty Targets)"
+                }
+            $result | Out-File -FilePath "$outputdir\SNMP_$clsname-$date.txt"
+            }
+        Write-Host "`nSNMP Settings have been modified for $clsname. Settings have been exported to $outputdir\SNMP_$clsname-$date.txt`n" -BackgroundColor Black -ForegroundColor Green
+    }
+        'q' {
+
+            # Exit the loop
+            Write-Host "`nClosing connections"
+            }
+    }
+} until($input -eq 'q')
+
+# Disconnect from vCenter
+Disconnect-VIServer * -Confirm:$false
